1efadfb57fa9262f31b469e4fcc8a9a54240e91e
evan_awn <evan.awn@outlook.com>
2023-05-27 09:48:19 +0800

    Add files via upload

---
+++ nginx-tls1.3.sh
@@ -0,0 +1,130 @@
+﻿#!/bin/bash
+
+# 更新系统软件包
+sudo apt update
+
+# 检查Python版本
+python_version=$(python3 -c "import sys; print('{}.{}'.format(sys.version_info.major, sys.version_info.minor))")
+required_python_version="3.7"
+
+if [[ $(printf "%s\n" "$required_python_version" "$python_version" | sort -V | head -n1) != "$required_python_version" ]]; then
+    echo "当前Python版本为$python_version，需要更新至$required_python_version或以上版本。"
+    echo "正在安装最新版本的Python..."
+    sudo apt install python3.9 -y
+    echo "Python已成功安装！"
+else
+    echo "当前Python版本已满足要求。"
+fi
+
+# 安装NGINX
+sudo apt install nginx -y
+
+# 检查是否已安装Python Certbot工具
+if ! command -v certbot &> /dev/null; then
+    sudo apt install certbot python3-certbot-nginx -y
+fi
+
+# 选择证书申请方式
+echo "请选择证书申请方式："
+echo "1. 默认向Let's Encrypt等CA申请证书"
+echo "2. 使用已有证书（客户提供）"
+read -p "请输入选项（1或2）: " cert_option
+
+if [ "$cert_option" == "2" ]; then
+    read -p "请输入已有证书的公钥文件路径: " cert_public_key_path
+    read -p "请输入已有证书的私钥文件路径: " cert_private_key_path
+
+    if [ ! -f "$cert_public_key_path" ] || [ ! -f "$cert_private_key_path" ]; then
+        echo "Error: 无效的证书公钥或私钥文件路径。将使用选项1默认申请证书。"
+        cert_option="1"
+    else
+        sudo cp "$cert_public_key_path" /etc/nginx/ssl/your-domain.com.crt
+        sudo cp "$cert_private_key_path" /etc/nginx/ssl/your-domain.com.key
+        echo "使用已有证书完成。"
+    fi
+fi
+
+if [ "$cert_option" == "1" ]; then
+    # 申请证书（使用certbot）
+    sudo certbot --nginx --non-interactive --agree-tos --redirect --hsts --staple-ocsp --email your-email@example.com -d your-domain.com
+    echo "证书签发机构：Let's Encrypt"
+fi
+
+# 获取证书有效期信息并显示
+cert_expiry=$(sudo openssl x509 -noout -dates -in /etc/letsencrypt/live/your-domain.com/fullchain.pem | grep "notAfter" | cut -d "=" -f 2)
+echo "SSL证书已签发，有效期至: $cert_expiry"
+
+# 创建404页面
+sudo bash -c 'cat << EOF > /var/www/html/404.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 Not Found</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 40px;
+        }
+        h1 {
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+        p {
+            font-size: 16px;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>404 Not Found</h1>
+    <p>Sorry, the page you requested was not found.</p>
+</body>
+</html>
+EOF'
+
+# 修改NGINX配置文件
+sudo bash -c 'cat << EOF > /etc/nginx/sites-available/default
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+    return 301 https://\$host\$request_uri;
+}
+
+server {
+    listen 443 ssl default_server;
+    listen [::]:443 ssl default_server;
+    server_name _;
+
+    ssl_certificate /etc/nginx/ssl/your-domain.com.crt;
+    ssl_certificate_key /etc/nginx/ssl/your-domain.com.key;
+
+    # 只接受TLS 1.3及以上版本的SSL协议
+    ssl_protocols TLSv1.3;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+    }
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /1080 {
+        proxy_pass http://localhost:1080;
+        proxy_set_header Host \$host;
+    }
+}
+EOF'
+
+# 创建SSL证书目录
+sudo mkdir /etc/nginx/ssl
+
+# 启用并启动NGINX服务
+sudo systemctl enable nginx
+sudo systemctl restart nginx
---
+++ rules.sh
@@ -0,0 +1,54 @@
+﻿#!/bin/bash
+
+# 清理iptables规则
+iptables -F
+iptables -X
+
+# 设置默认策略
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# 允许回环接口的流量
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# 允许已建立的、相关的进出流量
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# 开放80端口
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+
+# 开放443端口
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# 开放22端口
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# 查找已有的UDP规则
+existing_udp_rules=$(iptables -S INPUT | grep -E '^.* -p udp .*ACCEPT')
+
+# 添加UDP规则，如果不存在相同的规则
+iptables -C INPUT -p udp -j ACCEPT 2>/dev/null || iptables -A INPUT -p udp -j ACCEPT
+
+# 删除与已有UDP规则不同的规则
+for rule in $existing_udp_rules; do
+  iptables -D INPUT $rule
+done
+
+# 删除端口全开和端口全关规则
+iptables -D INPUT -j ACCEPT
+iptables -D INPUT -j DROP
+
+# 剔除重复规则
+iptables-save | awk '!x[$0]++' | iptables-restore
+
+# 输出清理结果
+echo "iptables规则已清理并更新："
+iptables -L
+
+# 保存规则
+iptables-save > /etc/iptables/rules.v4
+
+sudo netfilter-persistent save
+sudo netfilter-persistent reload
