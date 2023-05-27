b85ae1f27ba9ec1f289acc7dc6254c70648b90b5
evan_awn <evan.awn@outlook.com>
2023-05-27 10:16:07 +0800

    Add files via upload

---
+++ setup-ssh-root.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+
+# 启用SSH密码验证
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+# 允许root登录
+sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+# 重启SSH服务
+sudo service ssh restart
+
+# 设置root密码
+echo "设置root密码："
+sudo passwd root
+
+echo "SSH密码验证和root登录已启用，并且root密码已设置。"
